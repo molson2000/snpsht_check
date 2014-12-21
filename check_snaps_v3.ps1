10ea339e526a180301889557ec41857dae0325ea
molson2000 <olsonmike2000@gmail.com>
2014-12-21 15:06:34 -0500

    Create check_snaps_v3.ps1

---
+++ check_snaps_v3.ps1
@@ -0,0 +1,19 @@
+Add-PSSnapin VMware.VimAutomation.core
+#Email Config
+$SMTPserver = "192.168.50.40"
+$from = "nocgroup@transcendunited.com"
+$to = "mike.olson@transcendunited.com"
+#
+#Connect to Old vCenter check for snaps
+#
+    connect-viserver -server 0.0.0.0 -User username -Password password
+    $oldvcenter = foreach($vmview1 in get-view -viewtype virtualmachine){ $vmview1.Config.Hardware.Device | ? {$_ -is [VMware.Vim.VirtualDisk]} | %{$_.Backing | select @{N="VMname";E={$vmview1.name}},Filename |?{$_.FileName.Split('/')[-1] -match ".*\-[0-9]{6}\.vmdk"} } | out-string }; 
+	#foreach virtual machine get-view get only devices from its config that are hard disks, then select those that name matches pattern *-000000.vmdk 
+#
+#Email
+#
+$subject = "All Snapshots on All Datastores"
+$emailbody = "Old vCenter ______________________$oldvcenter New vCenter1 ________________________ $vcenter1  VFvCenter1 ________________________ $rdvcenter1 "
+$mailer = New-Object System.Net.Mail.SMTPclient($SMTPserver)
+$msg = New-Object System.Net.Mail.MailMessage($from, $to, $subject, $emailbody)
+$Mailer.send($msg)
